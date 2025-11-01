0c24953c893e737e80039a9e069b64baadcc1a9d
chicchiisk <chicchiisk@koromosoft.com>
2025-11-01 23:18:14 +0900

    first commit

---
+++ Source/UEKScript/UEKScript.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UEKScript.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UEKScript, "UEKScript" );
